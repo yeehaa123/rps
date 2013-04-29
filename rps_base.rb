434a852181521ba434cf7b76f53e44e2c38977ab
Yeehaa <medialoperations@gmail.com>
2013-04-29 15:43:53 +0200

    fix test, remove capybara

--- rps_base.rb
+++ rps_base.rb
@@ -1,27 +1,22 @@
 require 'sinatra'
-require 'rspec'
-require 'rack/test'
 require 'pry'
+require 'spec_helper'
 require './rps_game'
-require 'capybara'
+
 get '/' do
   @title = "rock, paper, scissors"
-  @output = Game.new.start("rock")
+  @output = Game.new.start "rock"
+  @welcome = "welcome to rock, paper, scissors"
   if params[:guess]
-    erb "Scissors"
-  else
     erb :result
+  else
+    erb :welcome
   end
 end
 
 set :environment, :test
 
-describe 'Rock Paper Scissors App', :type => :feature do
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
-  end
+describe 'Rock Paper Scissors App' do
 
   it "says welcome to rock, paper, scissors" do
     get '/'
@@ -29,10 +24,9 @@ describe 'Rock Paper Scissors App', :type => :feature do
     last_response.body.should include "welcome to rock, paper, scissors"
   end
 
-  it "when I say rock it should respond with either win, lose, or draw" do
+  it "when I say rock it should respond with either user wins, computer wins, or noone wins" do
     get '/', guess: "Rock"
-    last_response.body.should include "Scissors"
-    page.should have_selector('h1', text: "win")
+    last_response.body.should include "user"
   end
 end
 
@@ -57,3 +51,6 @@ __END__
 
 @@result
 <h1><%= @output %></h1>
+
+@@welcome
+<h1><%= @welcome %></h1>
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rack/test'
+include Rack::Test::Methods
+
+def app
+  Sinatra::Application 
+end
+
+set :environment, :test
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+end
